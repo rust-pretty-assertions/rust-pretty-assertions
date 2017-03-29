fe9270cc9006ec8a4cf7e86fc7eb03e0dcf4a237
Colin Kiegel <kiegel@gmx.de>
2017-03-29 22:34:16 +0200

    v0.1.2 - fix #3 (custom panic message)

--- src/lib.rs
+++ src/lib.rs
@@ -85,5 +85,20 @@ macro_rules! assert_eq {
                 }
             }
         }
-    })
+    });
+    ($left:expr , $right:expr, $($arg:tt)*) => ({
+        match (&($left), &($right)) {
+            (left_val, right_val) => {
+                if !(*left_val == *right_val) {
+                    let left_dbg = format!("{:?}", *left_val);
+                    let right_dbg = format!("{:?}", *right_val);
+                    let diff = $crate::Changeset::new(&left_dbg, &right_dbg, " ");
+
+                    panic!("assertion failed: `(left == right)` \
+                           (left: `{}`, right: `{}`, diff: `{}`): {}", left_dbg, right_dbg, diff,
+                           format_args!($($arg)*))
+                }
+            }
+        }
+    });
 }
