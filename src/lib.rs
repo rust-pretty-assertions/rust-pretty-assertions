15c42077cf1f266415f23d2fa33ab585306968f7
Florent Fayolle <florent.fayolle69@gmail.com>
2017-06-25 17:41:02 +0200

    Remove duplication for assert_ne!

--- src/lib.rs
+++ src/lib.rs
@@ -124,38 +124,34 @@ macro_rules! assert_eq {
 }
 
 #[macro_export]
-macro_rules! assert_ne {
-    ($left:expr, $right:expr) => ({
-        match (&$left, &$right) {
-            (left_val, right_val) => {
-                if *left_val == *right_val {
-                    panic!("assertion failed: `(left != right)`\
-                          \n\
-                          \n{}:\
-                          \n{:#?}\
-                          \n\
-                          \n",
-                           $crate::Style::new().bold().paint("Both sides"),
-                           left_val)
-                }
-            }
-        }
-    });
-    ($left:expr, $right:expr, $($arg:tt)+) => ({
+#[doc(hidden)]
+macro_rules! __assert_ne {
+    ($left:expr, $right:expr, $maybe_semicolon:expr, $($arg:tt)+) => ({
         match (&($left), &($right)) {
             (left_val, right_val) => {
                 if *left_val == *right_val {
-                    panic!("assertion failed: `(left != right)`: {}\
+                    panic!("assertion failed: `(left != right)`{}{}\
                           \n\
                           \n{}:\
                           \n{:#?}\
                           \n\
                           \n",
+                           $maybe_semicolon,
                            format_args!($($arg)+),
                            $crate::Style::new().bold().paint("Both sides"),
                            left_val)
                 }
             }
         }
+    });
+}
+
+#[macro_export]
+macro_rules! assert_ne {
+    ($left:expr, $right:expr) => ({
+        __assert_ne!($left, $right, "", "");
+    });
+    ($left:expr, $right:expr, $($arg:tt)+) => ({
+        __assert_ne!($left, $right, ": ", $($arg)+);
     });
 }
