71660186fd50d661d973a95387992242986bb782
Chris Butler <chrisbutler296@gmail.com>
2017-09-11 09:40:19 -0600

    Removed 'return' usage from assert_ne!()

--- src/lib.rs
+++ src/lib.rs
@@ -129,40 +129,38 @@ macro_rules! __assert_ne {
     ($left:expr, $right:expr, $maybe_semicolon:expr, $($arg:tt)+) => ({
         match (&($left), &($right)) {
             (left_val, right_val) => {
-                if *left_val != *right_val {
-                    return;
+                if *left_val == *right_val {
+                  let left_dbg = format!("{:?}", *left_val);
+                  let right_dbg = format!("{:?}", *right_val);
+                  if left_dbg != right_dbg {
+
+                      panic!("assertion failed: `(left != right)`{}{}\
+                            \n\
+                            \n{}\
+                            \n{}: According to the `PartialEq` implementation, both of the values \
+                              are partially equivalent, even if the `Debug` outputs differ.\
+                            \n\
+                            \n",
+                             $maybe_semicolon,
+                             format_args!($($arg)+),
+                             $crate::Comparison::new(left_val, right_val),
+                             $crate::Style::new()
+                                 .bold()
+                                 .underline()
+                                 .paint("Note"))
+                  }
+
+                  panic!("assertion failed: `(left != right)`{}{}\
+                        \n\
+                        \n{}:\
+                        \n{:#?}\
+                        \n\
+                        \n",
+                         $maybe_semicolon,
+                         format_args!($($arg)+),
+                         $crate::Style::new().bold().paint("Both sides"),
+                         left_val)
                 }
-
-                let left_dbg = format!("{:?}", *left_val);
-                let right_dbg = format!("{:?}", *right_val);
-                if left_dbg != right_dbg {
-
-                    panic!("assertion failed: `(left != right)`{}{}\
-                          \n\
-                          \n{}\
-                          \n{}: According to the `PartialEq` implementation, both of the values \
-                            are partially equivalent, even if the `Debug` outputs differ.\
-                          \n\
-                          \n",
-                           $maybe_semicolon,
-                           format_args!($($arg)+),
-                           $crate::Comparison::new(left_val, right_val),
-                           $crate::Style::new()
-                               .bold()
-                               .underline()
-                               .paint("Note"))
-                }
-
-                panic!("assertion failed: `(left != right)`{}{}\
-                      \n\
-                      \n{}:\
-                      \n{:#?}\
-                      \n\
-                      \n",
-                       $maybe_semicolon,
-                       format_args!($($arg)+),
-                       $crate::Style::new().bold().paint("Both sides"),
-                       left_val)
             }
         }
     });
